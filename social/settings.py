e512d947bc9424bf61a80a09fbe9c8d4cbfa283e
Amirreza Jamshidi <amir010101developer@gmail.com>
2024-03-29 15:51:31 +0330

    add reset and change password

--- social/settings.py
+++ social/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'socialapp.apps.SocialappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'socialapp.apps.SocialappConfig',
 ]
 
 MIDDLEWARE = [
